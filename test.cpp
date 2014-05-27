a69672ede97337ef79c0136c327577e0fa69c67c
root <root@infosec.(none)>
2014-05-27 22:45:49 +0800

    push

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: sandy
+	> Mail: sandy@luo.bo 
+	> Created Time: Tue 27 May 2014 10:43:50 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello World"<<endl;
+	return 0;
+}
